b10e77acccaf041422cf7ee8627109693f9b99c9
Jamie Barton <jamie@notrab.dev>
2021-12-17 15:01:54 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { loadSchemaSync } = require("@graphql-tools/load");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
+const faker = require("faker");
+
+const typeDefs = loadSchemaSync("schema.graphql", {
+  loaders: [new GraphQLFileLoader()],
+});
+
+const products = Array.from({ length: 10 }, (_, i) => ({
+  id: i + 1,
+  name: faker.commerce.productName(),
+  description: faker.commerce.productDescription(),
+  price: faker.commerce.price(),
+}));
+
+const resolvers = {
+  Query: {
+    product: (_, { id }) => products.find((product) => product.id === id),
+    products: () => products,
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  enableLogging: false,
+});
+
+server.start(() => console.log("Server is running on localhost:4000"));
